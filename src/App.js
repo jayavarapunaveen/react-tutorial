d1b4bfbd4a7c87785e5290bf4f35b491ca87f3e9
naveen <jayavarapunaveen@gmail.com>
2021-05-06 23:28:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// state and props in class based component
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: 'naveen',
+      lastName: ''
+
+    }
+    console.log('class is created', this.props.personName);
+    this.printName = this.printName.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+
+  }
+
+  printName(event) {
+    console.log(this.state);
+  }
+
+  handleChange(event){
+    let temp = {};
+    temp[event.target.name] = event.target.value;
+    //spread operator
+    this.setState({
+      ...temp
+    });
+  }
+
+
+  render() {
+    return (
+      <div>
+
+        <h1>Hello {this.props.personName} </h1>
+        <input type="text" name="firstName" value={this.state.firstName} onChange={this.handleChange} />
+        <input type="text" name="lastName" value={this.state.lastName} onChange={this.handleChange} />
+        <button onClick={this.printName}>print</button>
+      </div>
+    );
+  }
 }
+
+
+
+// function App(props) {
+//   return (
+//     <div className="App">
+//      Hello {props.personName}
+//     </div>
+//   );
+// }
 
 export default App;
