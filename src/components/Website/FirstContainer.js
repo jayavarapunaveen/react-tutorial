97f245757cf05dab5e3dbfd88f13c286383bfb99
naveen <jayavarapunaveen@gmail.com>
2021-07-03 01:20:21 +0530

    image crousel

--- src/components/Website/FirstContainer.js
+++ src/components/Website/FirstContainer.js
@@ -7,7 +7,20 @@ class FirstContainer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            showMenu: false
+            showMenu: false,
+            currentIndex: 0,
+            images: [{
+                imgUrl: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
+                text: "This is the first image"
+            },
+            {
+                imgUrl: "https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg",
+                text: "This is the second image"
+            },
+            {
+                imgUrl: "https://www.awakenthegreatnesswithin.com/wp-content/uploads/2018/08/Nature-Quotes-1.jpg",
+                text: "This is the third image"
+            }]
         }
 
     }
@@ -15,7 +28,15 @@ class FirstContainer extends React.Component {
     toggleMenu = () => {
         this.setState({ showMenu: !this.state.showMenu })
     }
-
+    changeSlide = (action) => {
+        if (action == 'P') {
+            this.setState({ currentIndex: this.state.currentIndex - 1 })
+        } else if (action == 'N') {
+            this.setState({ currentIndex: this.state.currentIndex + 1 })
+        } else {
+            this.setState({ currentIndex: action })
+        }
+    }
 
 
     render() {
@@ -27,8 +48,15 @@ class FirstContainer extends React.Component {
                 <p>Welcome to my website</p>
             </div>
             <div >
-                <h2 style={{ position: "absolute", top: "50%", color: "green", marginLeft: "30%" }}>MORE THAN 387,000 ENGAGED BY CHILDREN’S FEEDING</h2>
-                <img style={{ width: "100%", height: 700 }} src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" />
+                <button disabled={this.state.currentIndex == 0} style={{ position: "absolute", top: "50%", left: 0 }} onClick={() => this.changeSlide('P')}>P</button>
+                <button disabled={this.state.currentIndex == this.state.images.length - 1} style={{ position: "absolute", top: "50%", right: 0 }} onClick={() => this.changeSlide('N')}>N</button>
+                <div className="dot-container">
+                    {this.state.images.map((obj, index) => {
+                        return (<span className="dot" key={obj.imgUrl} onClick={() => this.changeSlide(index)}>&nbsp;&nbsp;&nbsp;</span>)
+                    })}
+                </div>
+                <h2 style={{ position: "absolute", top: "50%", color: "green", marginLeft: "30%" }}>{this.state.images[this.state.currentIndex].text}</h2>
+                <img style={{ width: "100%", height: 700 }} src={this.state.images[this.state.currentIndex].imgUrl} />
 
 
             </div>
